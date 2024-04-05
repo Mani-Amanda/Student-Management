3e5044cb85bccedf53c59e30744202a908d36e32
Mani-Amanda <135225894+Mani-Amanda@users.noreply.github.com>
2024-04-05 18:14:12 +0530

    Class function: Display, Sraech, Delete

--- code_1.py
+++ code_1.py
@@ -6,7 +6,22 @@ class Student:
         self.m1=m1
         self.m2=m2
     
-    def accept(self, Name, Rollno, marks1, marks2):
+    def accept(self, Name, Rollno, marks1, marks2): #create new student
         ob = Student(Name, Rollno, marks1, marks2)
         ls.append(ob)
+
+    def display(self, ob):  #display student details
+        print("Name : ", ob.name)
+        print("RollNo : ", ob.rollno)
+        print("Marks1 : ", ob.m1)
+        print("Marks2 : ", ob.m2)
+        print("\n") 
     
+    def search(self, rn):   #Search student
+        for i in range(ls.__len__()):
+            if(ls[i].rollno == rn):
+                return i
+    
+    def delete(self, rn):   #Delete student
+        i = obj.search(rn)
+        del ls[i]
